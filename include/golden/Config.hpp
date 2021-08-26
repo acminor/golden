361073aaeb6ac0b92f59eb36e36eb0cd8e046979
acminor <austin.chase.m@gmail.com>
2021-08-26 10:50:45 -0400

    Bump version to 0.3.1

--- include/golden/Config.hpp
+++ include/golden/Config.hpp
@@ -6,10 +6,10 @@
 #define GOLDEN_CONFIG_HPP
 
 #define golden_NAME "golden"
-#define golden_VERSION "0.3.0"
+#define golden_VERSION "0.3.1"
 #define golden_VERSION_MAJOR "0"
 #define golden_VERSION_MINOR "3"
-#define golden_VERSION_PATCH "0"
+#define golden_VERSION_PATCH "1"
 
 #define golden_GOLDEN_STORAGE_PATH "./GOLDEN"
 
